32e9bdb37a3070eb9e4b14b9975283c78f8cd668
matt <matt@griptape.ai>
2024-12-20 14:47:27 -0500

    Wiki format actions

--- src/helpers/link-replacer.ts
+++ src/helpers/link-replacer.ts
@@ -1,13 +1,16 @@
 import {existsSync} from "fs"
 import {extname} from "path"
 import transformLinks from "transform-markdown-links"
+import { debug } from "@actions/core"
 
 class LinkReplacer {
 
   private readonly filesPath: string
+  private readonly removeSubdirectories: boolean
 
-  constructor(filesPath: string) {
+  constructor(filesPath: string, removeSubdirectories: boolean) {
     this.filesPath = filesPath
+    this.removeSubdirectories = removeSubdirectories
   }
 
   protected safeDecodeURIComponent(str: string): string {
@@ -35,18 +38,27 @@ class LinkReplacer {
 
   processLink(link: string): string {
     const [potentialEncodedFile, fragment] = this.extractLinkParts(link)
-    const potentialFile = this.safeDecodeURIComponent(potentialEncodedFile)
+    let potentialFile = this.safeDecodeURIComponent(potentialEncodedFile)
     const fullPath: string = this.filesPath + '/' + potentialFile
 
     if (!existsSync(fullPath)) {
+      debug('File not found: ' + fullPath)
       return link
     }
 
     if (extname(fullPath) !== '.md') {
+      debug('Not a markdown file: ' + fullPath)
       return link
     }
 
+    if (this.removeSubdirectories) {
+      debug('Removing subdirectories: ' + potentialFile)
+      potentialFile = potentialFile.split('/').pop() as string
+    }
+
+    
     const uriPath = encodeURIComponent(potentialFile.substring(0, potentialFile.length - 3))
+    debug('Replacing link: ' + link + ' with ' + uriPath)
     if(!fragment){
       return uriPath
     }
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,69 @@
 import readDirSync from 'recursive-readdir-sync'
 import {readFileSync, writeFileSync} from 'fs'
-import {debug, getInput} from '@actions/core'
+import {getInput, info, error, setFailed} from '@actions/core'
 import LinkReplacer from './helpers/link-replacer'
 
-const filesPath: string = getInput('path', {required: true})
+export function run(): void {
+  const filesPath: string = getInput('path', {required: true})
+  const removeSubdirectories: boolean =
+    getInput('removeSubdirectories') === 'true'
+  const failOnDuplicateNames: boolean =
+    getInput('failOnDuplicateNames') === 'true'
+  info('Processing files in ' + filesPath)
+  info('Remove subdirectories: ' + removeSubdirectories)
+  info('Fail on duplicate names: ' + failOnDuplicateNames)
 
-const replacer = new LinkReplacer(filesPath)
+  const replacer = new LinkReplacer(filesPath, removeSubdirectories)
 
-for (const file of readDirSync(filesPath)) {
-  const filename: string = file.toString()
-  const oldContent: string = readFileSync(filename, 'utf8')
-  const newContent: string = replacer.transformMarkdownLinks(oldContent)
+  const fileNames: {[key: string]: string} = {}
 
-  if (oldContent != newContent) {
-    debug(filename + ' updated')
-    writeFileSync(filename, newContent)
+  for (const file of readDirSync(filesPath)) {
+    const filename: string = file.toString()
+    // get just the file name and put it in the fileNames object
+    const name = filename.split('/').pop() ?? ''
+    // map the file name to the full path
+    if (fileNames[name]) {
+      info(
+        'Duplicate file name: ' +
+          name +
+          ' at ' +
+          filename +
+          ' and ' +
+          fileNames[name]
+      )
+      if (failOnDuplicateNames) {
+        error(
+          'Duplicate file name: ' +
+            name +
+            ' at ' +
+            filename +
+            ' and ' +
+            fileNames[name]
+        )
+        setFailed(
+          'Duplicate file name: ' +
+            name +
+            ' at ' +
+            filename +
+            ' and ' +
+            fileNames[name]
+        )
+        return
+      }
+    }
+    fileNames[name] = filename
+  }
+
+  for (const file of readDirSync(filesPath)) {
+    const filename: string = file.toString()
+    const oldContent: string = readFileSync(filename, 'utf8')
+    const newContent: string = replacer.transformMarkdownLinks(oldContent)
+
+    if (oldContent != newContent) {
+      info(filename + ' updated')
+      writeFileSync(filename, newContent)
+    }
   }
 }
+
+run()
